baa8c6f67f6efec6c47a5d71f197371d6c469906
Francisco <franciscolopezjr012@gmail.com>
2021-09-07 01:54:38 -0500

    JavaScript code

---
+++ app.js
@@ -0,0 +1,24 @@
+const select = document.querySelector('select');
+const list = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+select.onchange = function() {
+    const choice = select.value;
+
+    
+
+
+    createCalendar(days, choice);
+}
+
+function createCalendar(days, choice) {
+  list.innerHTML = '';
+  h1.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+createCalendar(31,'January');
